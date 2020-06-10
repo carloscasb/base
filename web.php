f646cfdbe75b3292834de0c7feb9ce1ef8d0340f
carloscasb <54671330+carloscasb@users.noreply.github.com>
2020-06-10 20:41:51 -0300

    Add files via upload
    MODULO PESSOA COM CONTA BANCÁRIA (falta editar e deletar c\c)

--- web.php
+++ web.php
@@ -12,28 +12,48 @@
 */
 
 
-
-//ROTAS DE PESSOAS
-
-Route::get('/admin/pessoas/create', 'Admin\PessoaController@create')->name('pessoas.create');
-
-Route::put('/admin/pessoas/{url}', 'Admin\PessoaController@update')->name('pessoas.update');
-
-Route::get('/admin/pessoas/{url}/edit', 'Admin\PessoaController@edit')->name('pessoas.edit');
-Route::any('/admin/pessoas/search', 'Admin\PessoaController@search')->name('pessoas.search');
-Route::delete('/admin/pessoas/{url}', 'Admin\PessoaController@destroy')->name('pessoas.destroy');
-Route::get('/admin/pessoas/{url}', 'Admin\PessoaController@show')->name('pessoas.show');
-Route::post('/admin/pessoas', 'Admin\PessoaController@store')->name('pessoas.store');
-
-Route::get('/admin/pessoas', 'Admin\PessoaController@index')->name('pessoas.index');
+Route::prefix('admin')
+        ->namespace('Admin')
+         //->middleware('auth')
+        ->group(function() {
 
 
+        //ROTAS DE contas
+    
+    Route::post('pessoas/{id}/contas', 'ContaPessoaController@store')->name('contas.pessoa.store');
+    Route::get('pessoas/{id}/contas/create', 'ContaPessoaController@create')->name('contas.pessoa.create');
+     Route::get('pessoas/{id}/contas', 'ContaPessoaController@index')->name('contas.pessoa.index');
 
-//ROTAS DE GERAIS
-Route::get('/admin/gerals', 'Admin\GeralController@index')->name('gerals.index');
+    //ROTAS DE PESSOAS
+
+    Route::get('pessoas/create', 'PessoaController@create')->name('pessoas.create');
+
+    Route::put('pessoas/{url}', 'PessoaController@update')->name('pessoas.update');
+    Route::get('pessoas/{url}/edite', 'PessoaController@edite')->name('pessoas.edite');  //EXPERIMENTAL
+    Route::get('pessoas/{url}/edit', 'PessoaController@edit')->name('pessoas.edit');
+    Route::any('pessoas/search', 'PessoaController@search')->name('pessoas.search');
+    Route::delete('pessoas/{url}', 'PessoaController@destroy')->name('pessoas.destroy');
+    Route::get('pessoas/{url}', 'PessoaController@show')->name('pessoas.show');
+    Route::post('pessoas', 'PessoaController@store')->name('pessoas.store');
+    
+    Route::get('pessoas', 'PessoaController@index')->name('pessoas.index');
+    
+    
+    
+    //ROTAS DE GERAIS
+    Route::get('/gerals', 'GeralController@index')->name('gerals.index');
+
+
+
+    //ROTAS DE base (home-dashboard)
+    Route::get('/', 'PessoaController@index')->name('admin.index');
+
+});
+
+
 
 //ROTAS DE Acessorios
-Route::get('/admin', 'Admin\PessoaController@index')->name('admin.index');
+//
 
 Route::get('/', function () {
     return view('welcome');
