4ff1684cc4c0c75fd252cdbcb76854fae5c664ef
carloscasb <54671330+carloscasb@users.noreply.github.com>
2020-06-12 11:24:42 -0300

    Add files via upload

--- web.php
+++ web.php
@@ -18,11 +18,27 @@ Route::prefix('admin')
         ->group(function() {
 
 
+        
+ //ROTAS DE TELEFONE
+
+
+    Route::delete('pessoas/{id}/telefones/{idtelefone}', 'TelefonePessoaController@destroy')->name('telefones.pessoa.destroy');
+    Route::get('pessoas/{id}/telefones/{idtelefone}', 'TelefonePessoaController@show')->name('telefones.pessoa.show');        
+    Route::put('pessoas/{id}/telefones/{idtelefone}', 'TelefonePessoaController@update')->name('telefones.pessoa.update');
+    Route::get('pessoas/{id}/telefones/{idtelefone}/edit', 'TelefonePessoaController@edit')->name('telefones.pessoa.edit');
+     Route::post('pessoas/{id}/telefones', 'TelefonePessoaController@store')->name('telefones.pessoa.store');
+    Route::get('pessoas/{id}/telefones/create', 'TelefonePessoaController@create')->name('telefones.pessoa.create');
+    Route::get('pessoas/{id}/telefones', 'TelefonePessoaController@index')->name('telefones.pessoa.index');            
+
         //ROTAS DE contas
-    
+
+    Route::delete('pessoas/{id}/contas/{idconta}', 'ContaPessoaController@destroy')->name('contas.pessoa.destroy');
+    Route::get('pessoas/{id}/contas/{idconta}', 'ContaPessoaController@show')->name('contas.pessoa.show');        
+    Route::put('pessoas/{id}/contas/{idconta}', 'ContaPessoaController@update')->name('contas.pessoa.update');
+    Route::get('pessoas/{id}/contas/{idconta}/edit', 'ContaPessoaController@edit')->name('contas.pessoa.edit');
     Route::post('pessoas/{id}/contas', 'ContaPessoaController@store')->name('contas.pessoa.store');
     Route::get('pessoas/{id}/contas/create', 'ContaPessoaController@create')->name('contas.pessoa.create');
-     Route::get('pessoas/{id}/contas', 'ContaPessoaController@index')->name('contas.pessoa.index');
+    Route::get('pessoas/{id}/contas', 'ContaPessoaController@index')->name('contas.pessoa.index');
 
     //ROTAS DE PESSOAS
 
