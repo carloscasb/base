0ad54a0823e9cbaf210b43bd375d39f027171bdf
carloscasb <54671330+carloscasb@users.noreply.github.com>
2020-06-20 20:44:41 -0300

    Add files via upload

--- Controllers/Admin/MenbroOrcrimController.php
+++ Controllers/Admin/MenbroOrcrimController.php
@@ -11,61 +11,98 @@ use Illuminate\Http\Request;
 class MenbroOrcrimController extends Controller
 {
     //
+   
+    protected $orcrim, $pessoa, $repository;
 
-    private $repository, $orcrim;
+    public function __construct(Orcrim $orcrim, Pessoa $pessoa, MembroOrcrim $membroorcrim  )
+    {
+        $this->orcrim = $orcrim;
+        $this->pessoa = $pessoa;
+        $this->repository= $membroorcrim;
 
-    public function __construct(MembroOrcrim $membroorcrim, Orcrim $orcrim)
-       {
-           $this->repository= $membroorcrim;
-           $this->orcrim = $orcrim;
+      //  $this->middleware(['can:orcrims']);
+    }
+
+     //TENTAIVAS INSERIR
+
+    
       
-        }
-
-
-        public function index($idOrcrim)
-        {
-          If(!$orcrim = $this->orcrim->where ('id', $idOrcrim)->first()) {         //SE NÃo ACHOU (recuperando pela url)
-            return redirect()->back();
-          }                                                                   //SE ACHOU (recuperando pela url)
-                 
-          // $membros = $pessoa->membros();
-          $membros = $orcrim->membros()->paginate(); 
-          
-        return view('admin.pages.orcrims.membros.index', [
-            'pessoa' => $orcrim,
-            'membros'=> $membros,
-
-            ]);
-
-            }
-
-
-              public function create($idOrcrim)
-                         
-              {
-                if (!$orcrim = $this->orcrim->where('id', $idOrcrim)->first()) {
-                    return redirect()->back();
-                }
+    
         
-                return view('admin.pages.orcrims.membros.create', [
-                    'orcrim' => $orcrim,
-                ]);
-            }
+      
 
-            public function store(Request $request, $idOrcrim )
-            {
-                if (!$orcrim = $this->orcrim->where('id', $idOrcrim)->first()) {
-                    return redirect()->back();
-                }
+    public function create()
+    {
+        return view('admin.pages.orcrims.membros.acesso');
+    }
+
+     //  public function store(StoreUpdateProfile $request)
+   public function store(Request $request, $idOrcrim )
+   {
+    if (!$orcrim = $this->orcrim->where('id', $idOrcrim)->first()) {
+        return redirect()->back();
+    
+        $orcrim->membros()->create($request->all());
+    
+        return redirect()->route('orcrims.membros.acesso', $orcrim->id);
+    }
+      //   $this->repository->create($request->all());
+
+       //  return redirect()->route('orcrims.membros.acesso');
+   }
+
+
+   // LISTAGEM DAS PESSOA DE UMA ORCRIM 
+
+   public function pessoas($idOrcrim)
+   {
+       if (!$orcrim = $this->orcrim->find($idOrcrim)) {
+           return redirect()->back();
+       }
+
+       $pessoas = $orcrim->pessoas()->paginate();
+
+       return view('admin.pages.orcrims.membros.membros', compact('orcrim', 'pessoas'));
+   }
+
+// LISTAGEM das ORCRIM  DE UMA  PESSOA
+   public function orcrims($idPessoa)
+   {
+       if (!$pessoa = $this->pessoa->find($idPessoa)) {
+           return redirect()->back();
+       }
+
+       $orcrims = $pessoa->orcrims()->paginate();
+
+       return view('admin.pages.pessoas.orcrims.orcrims', compact('pessoa', 'orcrims'));
+   }
+
+   //  VAMOS PEGAR TODOS com $this->pessoas->All(); ou paginate()  ---PESSOAS QUE PODEM ESTAR DISPONIVEIS
+   public function pessoasAvailable(Request $request, $idOrcrim)
+   {
+       if (!$orcrim = $this->orcrim->find($idOrcrim)) {
+           return redirect()->back();
+       }
+
+     // $filters = $request->except('_token');
+
+     // $pessoas = $orcrim->pessoasAvailable($request->filter);
+
+      // $pessoas = $this->pessoa->All();
+      $pessoas = $this->pessoa->paginate();
+
+       return view('admin.pages.orcrims.membros.acesso', compact('orcrim', 'pessoas', 'filters'));
+   }
+
+   public function attachPessoasOrcrim(Request $request, $idOrcrim)
+   {
+    
+   
+      
+   }
+
         
-                // $data = $request->all();
-                // $data['orcrim_id'] = $orcrim->id;
-                // $this->repository->create($data);
-                $orcrim->membros()->create($request->all());
-        
-                return redirect()->route('membros.orcrim.index', $orcrim->id);
-            }
-           
+
 
 
 
